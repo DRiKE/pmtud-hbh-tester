3707cccfb0c9683a9ba59ef8da915b27efea50ee
Luuk Hendriks <mail@luukhendriks.eu>
2019-03-30 22:20:16 +0100

    use netlink to get next hop

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate bincode;
-extern crate pnet;
-extern crate structopt;
 extern crate byteorder;
+extern crate pnet;
+extern crate pnetlink;
+extern crate structopt;
+#[macro_use]
+extern crate log;
+extern crate simplelog;
 
+use pnetlink::packet::netlink::NetlinkConnection;
+//use pnetlink::packet::route::route::{Route, RoutesIterator};
+use pnetlink::packet::route::link::{Link, Links};
+use pnetlink::packet::route::neighbour::{NeighbourFlags, NeighbourState, Neighbours};
 
 use byteorder::{ByteOrder, LittleEndian, NetworkEndian, WriteBytesExt};
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, MacAddr, NetworkInterface};
-use pnet::packet::ethernet::{MutableEthernetPacket, EtherTypes};
+use pnet::packet::ethernet::{EtherTypes, MutableEthernetPacket};
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv6::MutableIpv6Packet;
 use pnet::packet::MutablePacket;
@@ -18,8 +26,13 @@ use structopt::StructOpt;
 
 use std::net::Ipv6Addr;
 
+use simplelog::{Config, LevelFilter, TermLogger};
+
 #[derive(Debug, StructOpt)]
 struct Opt {
+    /// Verbose/logging
+    #[structopt(short = "v", parse(from_occurrences))]
+    verbose: u64,
     /// listening mode
     #[structopt(short = "l", long = "listen")]
     listen: Option<bool>,
@@ -54,9 +67,15 @@ impl HBH {
     }
     */
     fn serialize(&self) -> [u8; 8] {
-        [17, 0, self.opt_type, self.opt_length,
-        (self.mtu1 >> 8) as u8, (self.mtu1 & 0xff) as u8,
-        (self.mtu2 >> 8) as u8, (self.mtu2 & 0xff) as u8
+        [
+            17,
+            0,
+            self.opt_type,
+            self.opt_length,
+            (self.mtu1 >> 8) as u8,
+            (self.mtu1 & 0xff) as u8,
+            (self.mtu2 >> 8) as u8,
+            (self.mtu2 & 0xff) as u8,
         ]
     }
 }
@@ -79,16 +98,79 @@ fn base_packet(saddr: Ipv6Addr, daddr: Ipv6Addr, mtu1: u16) -> MutableEthernetPa
     ipv6.set_payload_length(8);
     ipv6.set_payload(&hbh.serialize());
 
-    let mut packet = MutableEthernetPacket::owned(vec![0; MutableEthernetPacket::minimum_packet_size() + ipv6.packet_size()]).unwrap();
+    let mut packet = MutableEthernetPacket::owned(vec![
+        0;
+        MutableEthernetPacket::minimum_packet_size()
+            + ipv6.packet_size()
+    ]).unwrap();
     packet.set_ethertype(EtherTypes::Ipv6);
     packet.set_payload(ipv6.packet_mut());
     packet
 }
 
+fn get_link_info(oif: String) -> Link {
+    let mut conn = NetlinkConnection::new();
+    let link = conn
+        .iter_links()
+        .unwrap()
+        .filter(|link: &Link| link.get_name().unwrap() == oif)
+        .next()
+        .unwrap();
+    link
+}
+
+fn get_neighbour_info(link: &Link) -> MacAddr {
+    let mut conn = NetlinkConnection::new();
+    let neighbours = conn
+        .iter_neighbours(Some(link))
+        .unwrap()
+        .collect::<Vec<_>>();
+
+    let next_hop = neighbours
+        .iter()
+        .filter(|&neighbour| {
+            neighbour.get_state() == NeighbourState::REACHABLE
+                && neighbour.get_flags().contains(NeighbourFlags::ROUTER)
+        }).next()
+        .unwrap();
+    next_hop.get_ll_addr().unwrap()
+}
+
+fn fill_routing_info(oif: &str) -> () {
+    let link = get_link_info(oif.to_string());
+    let next_hop = get_neighbour_info(&link);
+    let mtu1 = link.get_mtu().unwrap();
+    let smac = link.get_hw_addr().unwrap();
+    info!(
+        "using mtu {:?} and smac {:?}, next hop {:?}",
+        mtu1, smac, next_hop
+    );
+    // FIXME getting next_hop etc based on the v6 dst address
+    // is not yet possible because pnetlink does not support it (March 30 2019)
+    //
+    //for route in Route::iter_routes(&mut conn) {
+    //    println!("{:?}", route);
+    //}
+
+    // For now, fill info based on passed outgoing interface
+}
+
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+
+    match opt.verbose {
+        0 => TermLogger::init(LevelFilter::Warn, Config::default()).unwrap(),
+        1 => TermLogger::init(LevelFilter::Info, Config::default()).unwrap(),
+        2 | _ => TermLogger::init(LevelFilter::Debug, Config::default()).unwrap(),
+    };
+
+    info!("Passed options: {:?}", opt);
+
+    //TODO better error handling on non-existing interface names
+    //think where we should check this (probably with NetLink)
+    //perhaps fill_routing_info could return a Option<my_info_struct>
     let interface_name = opt.interface;
+    fill_routing_info(&interface_name);
     let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
 
     // Find the network interface with the provided name
@@ -99,11 +181,14 @@ fn main() {
         .next()
         .unwrap();
 
+    //TODO get source address from link interface, and dst from opt
+    //TODO implement overriding of defaults, e.g. dmac is derived from --interface but can be
+    //passed as --dmac (which then has preference)
+    //same for mtu
     let mut frame = base_packet("1::1".parse().unwrap(), "2::1".parse().unwrap(), 0x1234);
     frame.set_source(interface.mac_address());
     frame.set_destination(opt.dmac);
 
-    println!("{:?}", interface);
     let (mut tx, mut _rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unhandled channel type"),
@@ -113,7 +198,5 @@ fn main() {
         ),
     };
 
-    println!("1");
     tx.send_to(frame.packet_mut(), None);
-    println!("2");
 }
